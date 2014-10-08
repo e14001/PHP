0ca116e981213791b8de9185190226d68c7b239f
Jun Akamine <e14001@std.it-college.ac.jp>
2014-10-08 19:06:13 +0900

    form

---
+++ php1/chap4/form.php
@@ -0,0 +1,19 @@
+<?php
+
+var_dump($_POST);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>フォームの練習</title>
+</head>
+<body>
+	<form action="form.php" method="post">
+		
+		<input type="submit" value="データ送信">
+	</form>
+</body>
+</html>
