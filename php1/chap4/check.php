9edb5ab77608f676e7b5ee53bd316351e324f5c9
Jun Akamine <e14001@std.it-college.ac.jp>
2014-10-08 19:21:50 +0900

    check

---
+++ php1/chap4/check.php
@@ -0,0 +1,20 @@
+<?php
+
+var_dump($_POST);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>入力のチェック</title>
+</head>
+<body>
+
+<p>
+<a href="form.php">フォームに戻る</a>
+</p>
+	
+</body>
+</html>
