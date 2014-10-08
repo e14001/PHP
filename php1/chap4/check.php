546af709ef4791045dec40d5916aab0c6377865d
Jun Akamine <e14001@std.it-college.ac.jp>
2014-10-08 20:52:39 +0900

    追加

--- php1/chap4/check.php
+++ php1/chap4/check.php
@@ -2,6 +2,28 @@
 
 var_dump($_POST);
 
+$name = $_POST['name'];
+
+$password = $_POST['password'];
+
+$note = nl2br($_POST['note']);
+
+$seibetsu = array(1 =>"男性", 2 =>"女性", 9 =>"回答しない");
+
+$sex = "不明";
+
+if(isset($_POST['sex'])){
+	$sex = $seibetsu[$_POST['sex']];
+}
+
+$todoufuken = array(1 =>"北海道", 2 =>"青森県", 3 =>"岩手県", 47 =>"沖縄県");
+
+$prefecture = "不明";
+
+if(isset($_POST['pref'])){
+	$prefecture = $todoufuken[$_POST['pref']];
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +33,13 @@ var_dump($_POST);
 	<title>入力のチェック</title>
 </head>
 <body>
+<ul>
+	<li>名前:<?php echo $name; ?></li>
+	<li>パスワード:<?php echo $password; ?></li>
+	<li>備考:<br /><?php echo $note; ?></li>
+	<li>性別:<br ?><?php echo $sex; ?></li>
+	<li>都道府県:<br /><?php echo $prefecture; ?></li>
+</ul>
 
 <p>
 <a href="form.php">フォームに戻る</a>
--- php1/chap4/form.php
+++ php1/chap4/form.php
@@ -11,9 +11,35 @@ var_dump($_POST);
 	<title>フォームの練習</title>
 </head>
 <body>
-	<form action="form.php" method="post">
+	<form action="check.php" method="post">
+	
+	名前<br />
+	<input type="text" name="name"><br />
+	パスワード<br />
+	<input type="password" name="password"><br />
+	備考<br />
+	<textarea name="note" cols="40" rows="5">
+	</textarea><br />
+	性別<br />
+	<input type="radio" name="sex" value="1">男性
+	<input type="radio" name="sex" value="2">女性
+	<input type="radio" name="sex" value="9">回答しない<br />
+	都道府県<br />
+	<select name="pref">
+		<option value="1">北海道</option>
+		<option value="2">青森県</option>
+		<option value="3">岩手県</option>
+		<option value="47">沖縄県</option>
+	</select><br />
+	趣味<br />
+	<input type="checkbox" name="hobby[]" value="1">ネット
+	<input type="checkbox" name="hobby[]" value="2">読書
+	<input type="checkbox" name="hobby[]" value="3">ショッピング
+	<input type="checkbox" name="hobby[]" value="4">サイクリング
+	<input type="checkbox" name="hobby[]" value="5">投資
+	<br />
 		
-		<input type="submit" value="データ送信">
+	<input type="submit" value="データ送信">
 	</form>
 </body>
 </html>
