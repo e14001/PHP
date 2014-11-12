e3fd38050e685c359a0903055f8bcc49608ebaf9
Jun Akamine <e14001@std.it-college.ac.jp>
2014-11-12 20:01:19 +0900

    Say Hello

---
+++ php2/chap1/sayhello.php
@@ -0,0 +1,34 @@
+<?php 
+
+if(isset($_POST['submit'])){
+	$msg = "Hello,";
+	// 'user'というフォームパラメーターでサブミットされた何かを出力
+	$msg .= $_POST['user'];
+	$msg .= '!';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>フォームの確認Document</title>
+</head>
+<body>
+<?php 
+//フォームがサブミットされた場合に挨拶を出力する
+if(isset($_POST['submit'])){
+	print $msg . '<br />';
+}else{
+// そうでなければ、フォームを出力
+$script_name = $_SERVER['SCRIPT_NAME'];
+print <<<AAAA
+<form action="$script_name" method = 'post'>
+Your Name:<input type="text" name="user" /><br />
+<input type="submit" value="Say Hello" name='submit' />
+</form>
+AAAA;
+} 
+?>
+</body>
+</html>
